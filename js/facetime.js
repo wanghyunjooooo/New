5dececd5c6b3f7b1ae6dad7213160cd797ee605d
wanghyunjooooo <w2324@e-mirim.hs.kr>
2024-11-28 01:21:46 +0900

    js 이용해서 call.html로 data-name 넘기기

--- js/facetime.js
+++ js/facetime.js
@@ -15,3 +15,14 @@ function startCamera() {
   // 페이지 로드 시 카메라 시작
   window.onload = startCamera;
   
+  function navigateToCall(image) {
+    const name = image.getAttribute('data-name'); // 클릭된 이미지의 이름을 가져옴
+    window.location.href = `call.html?name=${name}`; // 쿼리 문자열로 전달
+}
+
+  // URL에서 'name' 파라미터를 가져오기
+  const urlParams = new URLSearchParams(window.location.search);
+  const name = urlParams.get('name'); // 'name' 값 가져오기
+
+  // 'name-display' id를 가진 h1 요소에 이름을 표시
+  document.getElementById('name-display').innerText = name;
